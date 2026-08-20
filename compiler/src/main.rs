51511790e55af700b5b0d67a0bea94729140f072
Jonas0o0 <jonas.facon@proton.me>
2026-08-20 16:34:32 +0200

    feat(compiler): implement lexer for JUMP language

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,3 +1,11 @@
+use crate::lexer::Lexer;
+
+mod lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let code = "let x = 10; poke(252, 1);";
+    let mut lexer = Lexer::new(code);
+    let tokens = lexer.tokenize();
+
+    println!("{:#?}", tokens);
 }
