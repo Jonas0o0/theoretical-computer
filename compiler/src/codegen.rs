280eb52245f87eb8b36c16bb34852e21ac5820e7
Jonas0o0 <jonas.facon@proton.me>
2026-08-23 11:11:02 +0200

    feat(codegen): add statement code generation
    (cherry picked from commit 0fa7d2eab2a2dd5dee079bb42c1ff8fae2e2a79b)

--- compiler/src/codegen.rs
+++ compiler/src/codegen.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use crate::ast::{Program, Stmt};
 
 pub struct SymbolTable {
     table: HashMap<String, u8>,
@@ -29,5 +30,47 @@ impl SymbolTable {
 
     pub fn lookup(&self, name: &str) -> Option<u8> {
         self.table.get(name).copied()
+    }
+}
+
+
+pub struct CodeGen {
+    symbols: SymbolTable,
+    functions: HashMap<String, Vec<Stmt>>,
+    output: Vec<String>,
+}
+
+impl CodeGen {
+    pub fn new() -> Self {
+        Self {
+            symbols: SymbolTable::new(),
+            functions: HashMap::new(),
+            output: Vec::new(),
+        }
+    }
+
+    fn emit(&mut self, instruction: &str) {
+        self.output.push(instruction.to_string());
+    }
+
+    pub fn compile(&mut self, program: &Program) -> Result<String, String> {
+        for stmt in &program.statements {
+            self.compile_statement(stmt)?;
+        }
+        Ok(self.output.join("\n"))
+    }
+
+    fn compile_statement(&mut self, stmt: &Stmt) -> Result<(), String> {
+        match stmt {
+            Stmt::Let { name, value } => self.compile_let(name, value),
+            Stmt::Assign { name, value } => self.compile_assign(name, value),
+            Stmt::Poke { address, value } => self.compile_poke(address, value),
+            Stmt::If { condition, body } => self.compile_if(condition, body),
+            Stmt::While { condition, body } => self.compile_while(condition, body),
+            Stmt::Loop { body } => self.compile_loop(body),
+            Stmt::InlineFn {name, body} => self.compile_InlineFn(name, body),
+            Stmt::FnCall(name) => self.compileFnCall(name),
+            _ => Err(format!("Compilation non implémentée pour cette instruction : {:?}", stmt)),
+        }
     }
 }
