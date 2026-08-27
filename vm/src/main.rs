99174a891c9a6a46c52847562e62712cff741ad3
Jonas0o0 <jonas.facon@proton.me>
2026-08-27 11:15:58 +0200

    feat(vm): align with memory map and improve game window

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -1,12 +1,13 @@
-use std::{env, fs, process};
-use std::io::stdout;
+use std::{env, fs, process, thread};
+use std::io::{stdout, Stdout};
 use std::time::Duration;
-use crossterm::cursor::MoveTo;
+use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::event::{poll, read, Event, KeyCode};
 use crossterm::execute;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
 use theoretical_computer::hardware::cpu::Cpu;
-use theoretical_computer::hardware::utils::{u8_to_byte, byte_to_u8};
+use theoretical_computer::hardware::utils::{u8_to_byte, byte_to_u8, U16};
 
 const COLOR_BTE_CASE: u8 = 124;
 const X_BYTE_CASE: u8 = 125;
@@ -40,10 +41,22 @@ fn main() {
 
     enable_raw_mode().unwrap();
 
+    let mut stdout = stdout();
+
+    execute!(
+        stdout,
+        EnterAlternateScreen,
+        Hide,
+        Clear(ClearType::All)
+    ).expect("Erreur lors de l'initialisation de l'écran");
+
     loop {
-        key_input(&mut cpu);
-        print_ram(&mut cpu);
+        if !key_input(&mut cpu) {
+            break;
+        }
+        print_ram(&mut cpu, &mut stdout);
         cpu.clock_tick(false);
+        thread::sleep(Duration::from_millis(5));
     }
 
     println!("--- ÉTAT DE LA RAM (Valeurs non nulles) ---");
@@ -70,14 +83,12 @@ fn main() {
     println!("Arrêt de la machine.");
 }
 
-fn key_input(cpu: &mut Cpu) {
+fn key_input(cpu: &mut Cpu) -> bool {
     if poll(Duration::from_millis(0)).unwrap() {
         if let Event::Key(key_event) = read().unwrap() {
             match key_event.code {
-                KeyCode::Char('q') => {
-                    disable_raw_mode().unwrap();
-                    println!("\nArrêt du programme.");
-                    process::exit(0);
+                KeyCode::Esc => {
+                    return false;
                 }
                 KeyCode::Up | KeyCode::Char('z') => cpu.ram.write(usize::from(u8_to_byte(KEY_MEMORY_CASE)), u8_to_byte(b'z')),
                 KeyCode::Down | KeyCode::Char('s') => cpu.ram.write(usize::from(u8_to_byte(KEY_MEMORY_CASE)), u8_to_byte(b's')),
@@ -87,29 +98,27 @@ fn key_input(cpu: &mut Cpu) {
             }
         }
     }
+    true
 }
 
-fn print_ram(cpu: &mut Cpu) {
+fn print_ram(cpu: &mut Cpu,stdout: &mut Stdout) {
     let color_byte = cpu.ram.read_output(u8_to_byte(COLOR_BTE_CASE));
     let x_byte = cpu.ram.read_output(u8_to_byte(X_BYTE_CASE));
     let y_byte = cpu.ram.read_output(u8_to_byte(Y_BYTE_CASE));
 
     let color = byte_to_u8(&color_byte);
-    let x = byte_to_u8(&x_byte);
-    let y = byte_to_u8(&y_byte);
+    let x = byte_to_u8(&x_byte) as u16;
+    let y = byte_to_u8(&y_byte) as u16;
 
     if color != 0 {
-        let mut stdout = stdout();
-
-        let _ = execute!(stdout, MoveTo(x as u16, y as u16));
 
         match color {
-            1 => print!("██"),
-            2 => print!("🍎"),
-            3 => print!("  "),
+            1 => execute!(stdout, MoveTo(x, y), Print("██")).unwrap(),
+            2 => execute!(stdout, MoveTo(x, y), Print("🍎")).unwrap(),
+            3 => execute!(stdout, MoveTo(x, y), Print("  ")).unwrap(),
             _ => {}
         }
 
-        cpu.ram.write(usize::from(u8_to_byte(252)), u8_to_byte(0));
+        cpu.ram.write(usize::from(u8_to_byte(COLOR_BTE_CASE)), u8_to_byte(0));
     }
 }
