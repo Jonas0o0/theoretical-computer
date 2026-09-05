4e6e597b653838ec353ce265dc5cb9b354ffcfb8
Jonas0o0 <Jonas0o0@users.noreply.github.com>
2026-09-05 10:38:18 +0000

    deploy: 2c10e7176bad168fefcf34930e44ca1a51a5e250

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-12d5ac23.js',
+                path_to_root + 'searchindex-57c64f8f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
